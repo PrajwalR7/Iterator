b6fee6394e3f33439af9cf35970004f0a355b13b
Prajwal R <rameshprajwal21@gmail.com>
2022-06-30 00:10:05 +0530

    added a feature which includes user to access previous element

--- .spec.js
+++ .spec.js
@@ -1,33 +1,52 @@
 const { expect, describe, test } = require('@jest/globals');
-const {Stream} = require('./iterator')
+const { Stream } = require('./iterator');
 describe('Stream', () => {
-    test('itreator', () => {
-        const obj = new Stream([4,5,6])
-        const res = obj.iterator();
-        expect(res).toEqual(4);
-    });
-    test('iterator for sets', () => {
-        const myset = new Set();
-        myset.add(1);
-        myset.add({a:10,b:20});
-        const obj = new Stream(myset);
-        const res = obj.iterator();
-        expect(res).toBeDefined;
-    });
+  test('itreator', () => {
+    const obj = new Stream([4, 5, 6]);
+    const res = obj.iterator();
+    expect(res).toEqual(4);
+  });
+  test('iterator for sets', () => {
+    const myset = new Set();
+    myset.add(1);
+    myset.add({ a: 10, b: 20 });
+    const obj = new Stream(myset);
+    const res = obj.iterator();
+    expect(res).toBeDefined;
+  });
+  test('previous element', () => {
+    const myset = new Set();
+    myset.add(1);
+    myset.add(2);
+    const obj = new Stream(myset);
+    const res1 = obj.iterator();
+    expect(res1).toBe(1);
+    obj.iterator();
+    const res2 = obj.previous();
+    expect(res2).toBe(1);
+  });
+  test('checking previous element for the very first element should throw error', () => {
+    const myset = new Set();
+    myset.add(1);
+    myset.add(2);
+    const obj = new Stream(myset);
+    let res = obj.previous();
+    expect(res).toBeUndefined();
+  });
 });
 
 describe('Should fail to execute when an empty collection is sent as input', () => {
-    test('iterator', () => {
-        const obj = new Stream();
-        // const res = obj.iterator()
-        expect(() =>{
-            obj.iterator()
-        }).toThrow("Collection has no element")
-    });
-    test('display', () => {
-        const obj = new Stream();
-        expect(() => {
-            obj.display()
-        }).toThrow("Collection has no element")
-    });
+  test('iterator', () => {
+    const obj = new Stream();
+    // const res = obj.iterator()
+    expect(() => {
+      obj.iterator();
+    }).toThrow();
+  });
+  test('display', () => {
+    const obj = new Stream();
+    expect(() => {
+      obj.display();
+    }).toThrow();
+  });
 });
--- iterator.js
+++ iterator.js
@@ -1,61 +1,80 @@
-function outer(){
-    let i = -1; 
-    function inner(){
-        return ++i;
-    }
-    return inner;
+function outer() {
+  let i = -1;
+  function inner() {
+    return ++i;
+  }
+  return inner;
 }
 
+class Stream {
+  constructor(collection) {
+    this.flag = true;
+    this.innerFunc = null;
+    this.collection = collection;
+  }
+  previous() {
+    let isSet = false;
+    let i = 0;
+    if (this.flag === true) {
+      this.innerFunc = outer();
+      this.flag = false;
+    } else i = this.innerFunc();
+    try {
+      if (this.collection[i - 1] === undefined) {
+        var arr = Array.from(this.collection);
+        isSet = true;
+      }
+    } catch (error) {
+      throw 'Collection has no previous element';
+    }
+    if (isSet === true) {
+      return arr[i - 1];
+    }
+    return this.collection[i - 1];
+  }
+  iterator() {
+    let i = 0;
+    if (this.flag === true) {
+      this.innerFunc = outer();
+      this.flag = false;
+    } else i = this.innerFunc();
+    let isSet = false;
+    try {
+      if (this.collection[i] === undefined) {
+        var arr = Array.from(this.collection);
+        isSet = true;
+      }
+    } catch (error) {
+      throw 'Collection has no element';
+    }
 
-class Stream{
-    constructor (collection){
-        this.flag = true;
-        this.innerFunc = null;
-        this.collection = collection;
+    if (isSet === true) {
+      return arr[i];
     }
-    iterator(){
-        if(this.flag === true){
-            this.innerFunc = outer();
-            this.flag = false;
-        }
-        let i = this.innerFunc();
-        let isSet = false;
-        try {
-            if(this.collection[i] === undefined){
-                var arr = Array.from(this.collection);
-                isSet = true;
-            }
-        } catch (error) {
-            throw "Collection has no element"
-        }
-        
-        if(isSet === true){
-            return arr[i];
-        }
-        return this.collection[i];
+    return this.collection[i];
+  }
+  display() {
+    let i = use();
+    let isSet = false;
+    try {
+      if (this.collection[i] === undefined) {
+        var arr = Array.from(this.collection);
+        isSet = true;
+      }
+    } catch (error) {
+      throw new Error('Collection has no element');
     }
-    display(){
-        let i = use();
-        let isSet = false;
-        try {
-            if(this.collection[i] === undefined){
-                var arr = Array.from(this.collection);
-                isSet = true;
-            }
-        } catch (error) {
-            throw new Error("Collection has no element"); 
-        }
-        if(this.collection[i] === undefined){
-            var arr = Array.from(this.collection);
-            isSet = true;
-        }
-        if(isSet === true){
-            console.log(arr);
-        }
-        console.log(this.collection)
+    if (this.collection[i] === undefined) {
+      var arr = Array.from(this.collection);
+      isSet = true;
     }
+    if (isSet === true) {
+      console.log(arr);
+    }
+  }
 }
 
 module.exports = {
-    Stream , outer
-}
+  Stream,
+  outer,
+};
